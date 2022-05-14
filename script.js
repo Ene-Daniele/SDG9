e199db0c267300c897069f8db9e6ce49fa62654c
Evryon <ene.daniele@iisgalvanimi.edu.it>
2022-05-14 18:17:09 +0200

    website done *
    just gotta replace the lorems with actual content now and make the buttons change the paragraph, find a use for that header, and maybe add some fancy transitioning animations

---
+++ script.js
@@ -0,0 +1,12 @@
+function bone(){
+    document.getElementById("body").style.backgroundImage = "url(bgs/bg1.jpg)";
+}
+function btwo(){
+    document.getElementById("body").style.backgroundImage = "url(bgs/bg2.jpg)";
+}
+function bthree(){
+    document.getElementById("body").style.backgroundImage = "url(bgs/bg3.jpg)";
+}
+function bfour(){
+    document.getElementById("body").style.backgroundImage = "none";
+}
