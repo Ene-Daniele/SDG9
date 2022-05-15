f9197bfd3974de4f56506b18f1df2fae05189f13
Evryon <ene.daniele@iisgalvanimi.edu.it>
2022-05-15 16:20:51 +0200

    ⨠ minor layout revamp

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//TODO Dont forget to make these buttons change the text accordingly lol, and also madd transitioning animations
 function bone(){
     document.getElementById("body").style.backgroundImage = "url(bgs/bg1.jpg)";
 }
@@ -10,3 +11,7 @@ function bthree(){
 function bfour(){
     document.getElementById("body").style.backgroundImage = "none";
 }
+document.getElementById("ball").addEventListener("click", () => {
+    //! Reset to original stuff and stuff you know what you mean
+    console.log("alskdj");
+})
